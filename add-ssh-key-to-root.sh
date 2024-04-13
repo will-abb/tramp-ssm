45c6b7f2fb9e0bb0aa67b91556eaa11bf2c9bbdd
Will Bosch-Bello <williamsbosch@gmail.com>
2024-04-12 22:24:07 -0500

    Commit automatically generated.

--- add-ssh-key-to-root.sh
+++ add-ssh-key-to-root.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# The path to the local public SSH key
-public_key_path="$HOME/.ssh/id_rsa.pub"
-
-# Ensure the public key file exists
-if [ ! -f "$public_key_path" ]; then
-    echo "Public key not found at $public_key_path"
-    exit 1
-fi
-
 # Read the local public SSH key
-public_key=$(<$public_key_path)
+public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCz5LW1RTI98Cykz8q+UtXkDlRV2KyWOMkrjM/iDs2x5XlZfKlVJqgaBmY1ahyJm1pJExgWMl6zFU0Q+wKKShkVZQokwDkCl49V7ZkFwaOY3fFSqpdLxTd03V+qdsZhHKQdxDUqaFJJBDcTitJUd5MEe5sIf2nklS+65aj6+IqtI2NKD1XzOWkPTGExv3NZFcljJpVAJa+/CrCTbkYiyBQCUuibojs1PcYOcb50nDaHOhPMY4fg4oacKpfFrK6t7PEtxt76oc5svG6RCYq7XyZ3TTXJx/CNXBNZZsp4BmT51GruDMl96ZMP5KvzLpk5PEXxFh9tOy4XlBlusR4vqmu6PSG8LBYPRHv8vARizUyiv0GVbG8Cm3Y+N1tYEjxczWYpzbYfhzAD6jQdoxoGkmoP4Rz3ezwkN4Aq/sqscaqvaDBPPTjI+KLHmfI056TA9Gs1frX0BgSDJ86pEwya6f/wI5lhfVWj5++zwHo2e5v1vO87Oa1ok8mfLRdKkOqw1KlHS5sJ8r8aOXdxGFH2rVtp/gdoTVGKM/sEbJZHzzC0jGtIzYfjgXxrgLIUi/OiqZxxZxRiljqFS1pTiP6anXANcJsUQ0L60jSrZRkImnh3yUW14sCpfLwVGTsEFvIM+gmGTZnCwCg07W3h/C8Y2Kjz6WDtHdGD6y63N4iPaXeRSQ== williams.bosch-bello@selectquote.com"
 
 # Append the public SSH key to the root's authorized_keys
 echo "$public_key" >> /root/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCz5LW1RTI98Cykz8q+UtXkDlRV2KyWOMkrjM/iDs2x5XlZfKlVJqgaBmY1ahyJm1pJExgWMl6zFU0Q+wKKShkVZQokwDkCl49V7ZkFwaOY3fFSqpdLxTd03V+qdsZhHKQdxDUqaFJJBDcTitJUd5MEe5sIf2nklS+65aj6+IqtI2NKD1XzOWkPTGExv3NZFcljJpVAJa+/CrCTbkYiyBQCUuibojs1PcYOcb50nDaHOhPMY4fg4oacKpfFrK6t7PEtxt76oc5svG6RCYq7XyZ3TTXJx/CNXBNZZsp4BmT51GruDMl96ZMP5KvzLpk5PEXxFh9tOy4XlBlusR4vqmu6PSG8LBYPRHv8vARizUyiv0GVbG8Cm3Y+N1tYEjxczWYpzbYfhzAD6jQdoxoGkmoP4Rz3ezwkN4Aq/sqscaqvaDBPPTjI+KLHmfI056TA9Gs1frX0BgSDJ86pEwya6f/wI5lhfVWj5++zwHo2e5v1vO87Oa1ok8mfLRdKkOqw1KlHS5sJ8r8aOXdxGFH2rVtp/gdoTVGKM/sEbJZHzzC0jGtIzYfjgXxrgLIUi/OiqZxxZxRiljqFS1pTiP6anXANcJsUQ0L60jSrZRkImnh3yUW14sCpfLwVGTsEFvIM+gmGTZnCwCg07W3h/C8Y2Kjz6WDtHdGD6y63N4iPaXeRSQ== williams.bosch-bello@selectquote.com
